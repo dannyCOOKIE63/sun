9ced8ed2c443b1ffd305cdb24871e294df1dd7b6
dannyCOOKIE63 <daniild2013@icloud.com>
2024-10-19 20:11:17 +0400

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,48 @@
+// alert("Привет, Даня! ")
+// console.log(
+//     "хай это моя первая программа"
+
+// )
+
+// console.log(
+//     "я люблю рисовать"
+// )
+
+// console.log(
+//     "я люблю творчество"
+// )
+
+// console.log(
+//     "я не люблю темнату"
+// )
+// let admin;
+// let name;
+// name="джон";
+// admin=name
+// alert(admin)
+//let num1;
+//let num2;
+//num1=53;
+//num2=3;
+//console.log(
+//num1+num2
+//)
+//console.log(
+//    num1-num2
+//)
+//console.log(
+//    num1/num2
+//)
+//console.log(
+//    num1*num2
+//)
+a=13102024
+b=123456789n
+c="648264926493754368974248053607247dany"
+d=true
+e=null
+f=undefined
+console.log(
+    typeof(a)
+)
+
